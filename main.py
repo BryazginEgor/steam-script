6c94d8bcd3ab76ae04c721957c914df660c91877
EgorBR <e.bryazgin@yandex.ru>
2023-12-02 19:23:00 +0300

    добавлено исключение

--- main.py
+++ main.py
@@ -15,18 +15,24 @@ def get_steam_paths(steam_library_folders):
                 steam_path.append(line[11:-2] + "\steamapps")
 
     return steam_path
+
+
 # Читает манифест файлы в каждой папке Steam\steamapps
 def get_manifest(steam_path):
-    # Для каждого файла в steam_path
-    for filename in os.listdir(steam_path):
-        # Получение общего пути для каждого файла
-        f = os.path.join(steam_path, filename)
-        # Если файл оканчивается на .acf
-        if os.path.isfile(f) and filename.endswith('.acf'):
-            with open(f, 'r') as file:
-                fileLines = file.readlines()
-                line = fileLines[7].strip()
-                print(line[15:-1])
+    try:
+        # Для каждого файла в steam_path
+        for filename in os.listdir(steam_path):
+            # Получение общего пути для каждого файла
+            f = os.path.join(steam_path, filename)
+            # Если файл оканчивается на .acf
+            if os.path.isfile(f) and filename.endswith('.acf'):
+                with open(f, 'r') as file:
+                    fileLines = file.readlines()
+                    line = fileLines[7].strip()
+                    print(line[15:-1])
+    # Если директория отсутствует
+    except FileNotFoundError as e:
+        print(f"No such directory: {e.filename}")
 
 steam_paths = get_steam_paths(steam_path)
 
